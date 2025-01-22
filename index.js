4e82e2dda003b2ff6b8dd03895d42f4f971f2b31
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-01-22 14:09:31 -0330

    added error handling and character sets

--- index.js
+++ index.js
@@ -3,13 +3,94 @@
 const args = process.argv.slice(1);
 
 const helpMessage = `
-Usage: qap [options] [arguments]
+Usage: index.js [options] [arguments]
 
 Options:
   --help, -h     Show this help message
-  --version, -v  Show the version number
   --caps, -c     Include capital letters
-  --numbers, -n  Include numbers
-  --special, -s  Include special characters
   --length, -l   Length of the password (default: 8)
 `;
+
+if (args.includes("--help") || args.includes("-h")) {
+  console.log(helpMessage);
+  process.exit(0);
+}
+
+function showErrorAndExit(message) {
+  console.error(`Error: ${message}`);
+  console.log('Use "index.js --help" for usage information.');
+  process.exit(1);
+}
+
+const lengthIndex =
+  args.indexOf("--length") !== -1
+    ? args.indexOf("--length") + 1
+    : args.indexOf("-l") + 1;
+
+let length = 8; // Default length
+if (lengthIndex > 0) {
+  if (!args[lengthIndex] || isNaN(args[lengthIndex])) {
+    showErrorAndExit(
+      "Invalid or missing value for --length. Please provide a valid number."
+    );
+  } else {
+    length = parseInt(args[lengthIndex]);
+  }
+}
+
+const smallSet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const largeSet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
